020a43d1a11d3ffc87adc338364a97bd3748601e
Jakub Ściślewski <jakub.scislewski@gmail.com>
2020-06-05 17:59:52 +0200

    Remove hardcoded localhost

--- projekt/auction-app/src/service/AuctionsService.js
+++ projekt/auction-app/src/service/AuctionsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "";
 
 axios.defaults.withCredentials = true;
 
--- projekt/auction-app/src/service/ChatService.js
+++ projekt/auction-app/src/service/ChatService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/chat";
+const url = "";
 
 class ChatService {
   static getMessages(user, user2) {
