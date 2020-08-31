53f1777b17c573543fa509a5718c4bcb63bd808d
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-09-01 00:38:30 +0200

    Add posting new user

--- server/routers/user.js
+++ server/routers/user.js
@@ -27,4 +27,16 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+
+    res.status(201).send({ user });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 module.exports = router;
