27d03d16972d6884cd3264f31c5382352fbc9f00
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-31 15:00:36 +0200

    Delete old version

--- server/config/database-config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("useFindAndModify", false);
-mongoose.connect(
-  "mongodb+srv://auctionuser:auctionuser@cluster0-vk8ez.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
-module.exports = mongoose;
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  devServer: {
-    proxy: "https://localhost:8443",
-    https: true,
-  },
-};
